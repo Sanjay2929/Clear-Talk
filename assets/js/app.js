320f91d10f9beb9f841c6303a2f2426e4869e737
radialcoderavipoonia <ravipoonia28122002@gmail.com>
2024-01-24 16:23:53 +0530

    year function

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,6 @@ function shownav() {
 // function hidenav() {
 //   document.getElementById("showul").classList.add("start-0");
 // }
+
+// YEAR SCRIPT 
+  document.getElementById('copyright').innerHTML = 'copyright&copy;' + new Date().getFullYear() + ' ClearTalk.ai - All Rights Reserved';
