62721c1839e8790c43e1eb1f2ba32eb42609251f
Sanjay2929 <Sanjayjyani220@gmail.com>
2024-01-24 21:10:46 +0530

    code merge

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,29 +3,33 @@ function shownav() {
   document.getElementById("showul").classList.toggle("nav_visible");
 }
 
-// SLIDER SOME OF OUR CALLS 
+// SLIDER SOME OF OUR CALLS
 $(".Slider").slick({
-  dots: true,
+  dots: false,
   infinite: true,
+  arrows: false,
   speed: 300,
+  centerMode: true,
   slidesToShow: 2,
   slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 3000,
+  nextArrow: ".slider_arrow_right",
+  prevArrow: ".slider_arrow_left",
+  focusOnSelect: true,
   responsive: [
     {
       breakpoint: 992,
       settings: {
-        slidesToShow: 2,
+        slidesToShow: 1.5,
         slidesToScroll: 1,
       },
     },
     {
-      breakpoint: 500,
+      breakpoint: 640,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
+        centerMode: false,
       },
     },
   ],
-}); 
+});
