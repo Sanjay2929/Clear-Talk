e13a44f7e301db72d37da305ad3e2ac756bbd262
Sanjay2929 <Sanjayjyani220@gmail.com>
2024-01-25 10:25:55 +0530

    code merge

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,30 +1,31 @@
 // SLIDER SOME OF OUR CALLS
 $(".Slider").slick({
-    dots: false,
-    infinite: true,
-    arrows: false,
-    speed: 300,
-    centerMode: true,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    nextArrow: ".slider_arrow_right",
-    prevArrow: ".slider_arrow_left",
-    focusOnSelect: true,
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 1.5,
-          slidesToScroll: 1,
-        },
+  // dots: false,
+  infinite: true,
+  arrows: false,
+  speed: 300,
+  centerMode: true,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  prevArrow: $(".slider_arrow_left"),
+  nextArrow: $(".slider_arrow_right"),
+
+  focusOnSelect: true,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 1.5,
+        slidesToScroll: 1,
       },
-      {
-        breakpoint: 640,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          centerMode: false,
-        },
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: false,
       },
-    ],
-  });
+    },
+  ],
+});
